27a8666ed80fa740c2fd5645594e465b8bd392f6
James Mead <james@floehopper.org>
2013-05-13 08:50:19 +0100

    WIP: hacked so unit tests pass.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,4 +8,21 @@ $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'unit'))
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'unit', 'parameter_matchers'))
 $:.unshift File.expand_path(File.join(File.dirname(__FILE__), 'acceptance'))
 
-require 'test/unit'
+# require 'test/unit'
+require 'minitest/autorun'
+module Test
+  module Unit
+    class TestCase < ::MiniTest::Test
+      def assert_nothing_raised(exception = StandardError)
+        yield
+      rescue exception => e
+        flunk "Unexpected exception raised: #{e}"
+      end
+
+      alias_method :assert_not_nil, :refute_nil
+      alias_method :assert_raise, :assert_raises
+      alias_method :assert_not_same, :refute_same
+      alias_method :assert_no_match, :refute_match
+    end
+  end
+end
